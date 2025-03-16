f17da72dca6087086de26702caa9b9d8792a0254
Christoph Schwarz <42540479+christophschwarz@users.noreply.github.com>
2025-03-16 14:39:16 +0100

    fixed login()

--- tado-assistant.sh
+++ tado-assistant.sh
@@ -50,7 +50,7 @@ handle_curl_error() {
 login() {
     local account_index=$1
     local refresh_token_var=$2
-    local old_refresh_token="${!token_var}"
+    local old_refresh_token="${!refresh_token_var}"
     local response expires_in token new_refresh_token
 
     response=$(curl -s -X POST "https://login.tado.com/oauth2/token" \
